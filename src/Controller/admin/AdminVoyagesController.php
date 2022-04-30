229fe848e797c2dd6dcb60f97530a5b5bc75c049
DIM <dimitrijacquart@gmail.com>
2022-05-01 01:09:58 +0200

    phase   : ORM (deuxième table 'environnement' et lien entre les deux tables, type association ManyToMany).".

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -2,12 +2,14 @@
 
 namespace  App\Controller\admin;
 
+use App\Entity\Visite;
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -51,14 +53,14 @@ class AdminVoyagesController extends AbstractController {
      * @param VisiteRepository $repository
      * @param EntityManagerInterface $om
      */
-    function __construct(VisiteRepository $repository, EntityManagetInterface $om) {
+    function __construct(VisiteRepository $repository, EntityManagerInterface $om) {
         $this->repository= $repository;
         $this->om = $om;
     }
     
     /**
-     * @Route ("/admin/suppr/{id]", name="admin.voyage.suppr")
-     * @param \App\Controller\admin\Visite $visite
+     * @Route ("/admin/suppr/{id}", name="admin.voyage.suppr")
+     * @param Visite $visite
      * @return Response
      */
     public function suppr(Visite $visite): Response {
@@ -67,4 +69,58 @@ class AdminVoyagesController extends AbstractController {
         return $this->redirectToRoute('admin.voyages');
         
     }
+    
+    /**
+     * @Route ("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param Visite $visite
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Visite $visite, Request $request): Response {
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid())
+            $this->om->flush();
+            return $this->redirectToRoute('admin.voyages');
+            
+        
+        
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+    ]);
+        
+     
+    
+    }   
+    
+   /**
+     * @Route("/admin/ajout", name="admin.voyage.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response{
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->om->persist($visite);
+            $this->om->flush();
+            return $this->redirectToRoute(self::ADMINVOYAGES);
+        }
+        
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);
+    }
+            
+            
+        
+            
+    
+    
 }
+
